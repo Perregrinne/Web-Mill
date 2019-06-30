a408bba6dbea5b45744aeb4b52ed28ad9da6c5a4
Perregrinne <ajacob779@gmail.com>
2019-06-29 19:36:09 -0600

    Rest of the files
    This should be pretty much all basic files and folders needed (but little or no code yet). If I remember something else, I'll add that too.

---
+++ php/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>  
+    <?php require ($_SERVER['DOCUMENT_ROOT'] . "/php/admin/config.php"); ?>
+    <title><?php echo $WEBSITE_TITLE; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/default-css.php">
+    <!--link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"-->
+    <?php echo '<link rel="shortcut icon" href="' . $_SERVER['DOCUMENT_ROOT'] . $FAVICON_FILE .  '" type="image/x-icon" >'; ?>
+</head>
+<body>
+    <div class="navbar">
+    </div>
+</body>
+</html>
