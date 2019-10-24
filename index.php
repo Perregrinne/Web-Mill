e44aa5358ca109b6b6cfdfb7334a39ab03c78f92
Perregrinne <ajacob779@gmail.com>
2019-10-23 19:58:39 -0600

    Proper read/write system
    I had an idea for how to make changes to a page and later read them, and it was a bad one that I just undid. I will try a much more proper way now.

--- index.php
+++ index.php
@@ -5,158 +5,37 @@
     <head>
         <?php
             include ($_SERVER['DOCUMENT_ROOT'] . "/php/header.php");
-
-            //Included scripts
-            $includes = [];
-
-            $includes['clock'] = '<script src="/javascript/clock.js"></script>';
-
-            //All href locations
-            $hrefs = [];
-            $hrefs['navbar-home-link'] = '/index.php';
-            $hrefs['navbar-admin-link'] = '/admin.php';
-
-            //All files
-            $files = [];
-            $files['navbar-logo'] = '/favicon.png';
-
-            //Tracks content type
-            $contentType = [];
-            $contentType['navbar'] = 'div';
-            $contentType['navbar-logo'] = 'img';
-            $contentType['navbar-title'] = 'div';
-            $contentType['navbar-home-link'] = 'href';
-            $contentType['navbar-admin-link'] = 'href';
-            $contentType['main-text'] = 'h1';
-            $contentType['clock'] = 'span';
-            $contentType['footer'] = 'div';
-            $contentType['copyright'] = 'div';
-            $contentType['copyright-text'] = 'div';
-            $contentType['copyright-year'] = 'div';
-            $contentType['copyright-name'] = 'div';
-
-            //All of the content found in the body of the html
-            $bodyContent = [];
-            //Nested element of bodyContent
-            $navbarContent = [];
-            //Nested element of bodyContent
-            $footerContent = [];
-            //Nested element of footerContent
-            $copyrightContent = [];
-
-            $copyrightContent['copyright-text'] = 'Copyright' . '&nbsp&copy&nbsp';
-            $copyrightContent['copyright-year'] = date('Y');
-            $copyrightContent['copyright-name'] = '&nbsp' . 'Your Name Here';//$ORGANIZATION;
-
-            $footerContent['copyright'] = $copyrightContent;
-            
-
-            $navbarContent['navbar-logo'] = '/favicon.png';
-            $navbarContent['navbar-title'] = 'Web Mill';//$ORGANIZATION;
-            $navbarContent['navbar-home-link'] = 'home';
-            $navbarContent['navbar-admin-link'] = 'admin';
-
-            $bodyContent['navbar'] = $navbarContent;
-            $bodyContent['main-text'] = 'Welcome to your website!';
-            $bodyContent['clock'] = '<script src="/javascript/clock.js" onload="getTime(); setInterval(\'getTime()\', 1000);"></script>';
-            $bodyContent['footer'] = $footerContent;
-
         ?>
     </head>
     <body id="body" class="nested">
-        <?php
-            while ($elem = current($bodyContent))
-            {
-                $idKey = key($bodyContent);
-                displayElem($idKey, $contentType[$idKey], $elem);
-                next($bodyContent);
-            }
-            /*
-            echo '<div class="nested" id="main-text">This is your new website!</div>';
-            echo '<script src="/javascript/clock.js"></script>';
-            echo '<br>';
-            echo '<span class="nested" id="clock"></span>';
-            echo '<br>';
-            echo '<a href="/admin.php" class="nested" id="admin-link" style="position: absolute;">Admin</a>';
-            echo '<div class="nested" id="nestBoxTest" style="position: absolute; left: 300px; top: 100px; height: 500px; width: 500px; background-color: green;">&nbsp</div>';
-            */
-
-            function displayElem($elemId, $elemType, $elemContent)
-            {
-                global $hrefs;
-                global $files;
-                global $includes;
-
-                switch ($elemType)
-                {
-                    case 'div':
-                        echo '<div class="nested" id="' . $elemId . '">';
-                        if(is_array($elemContent))
-                        {
-                            getElem($elemContent);
-                        }
-                        else
-                        {
-                            echo $elemContent;
-                        }
-                        echo '</div>';
-                        break;
-                    case 'span':
-                        echo '<span class="nested" id="' . $elemId . '">';
-                        if(is_array($elemContent))
-                        {
-                            getElem($elemContent);
-                        }
-                        else
-                        {
-                            echo $elemContent;
-                        }
-                        echo '</span>';
-                        break;
-                    case 'h1':
-                        echo '<h1 class="nested" id="' . $elemId . '">';
-                        if(is_array($elemContent))
-                        {
-                            getElem($elemContent);
-                        }
-                        else
-                        {
-                            echo $elemContent;
-                        }
-                        echo '</h1>';
-                        break;
-                    case 'href':
-                        echo '<a href="' . $hrefs[$elemId] . '" class="nested" id="' . $elemId . '">';
-                        if(is_array($elemContent))
-                        {
-                            getElem($elemContent);
-                        }
-                        else
-                        {
-                            echo $elemContent;
-                        }
-                        echo '</a>';
-                        break;
-                    case 'img':
-                        echo '<img src="' . $files[$elemId] . '" class="nested" id="' . $elemId . '">';
-                        break;
-                    case 'script':
-                        echo '<script src="' . $includes[$elemId] . '" class="nested" id="' . $elemId . '"></script>';
-                        break;
-                }
-            }
-
-            function getElem($content)
-            {
-                global $contentType;
-
-                while ($elem = current($content))
-                {
-                    $idKey = key($content);
-                    displayElem($idKey, $contentType[$idKey], $elem);
-                    next($content);
-                }
-            }
-        ?>
+        <span id="clock" class="nested"><script src="/javascript/clock.js" onload="getTime(); setInterval('getTime()', 1000);"></script></span>
+        <div class="nested" id="navbar">
+            <img src="/favicon.png" class="nested" id="navbar-logo">
+            <div class="nested" id="navbar-title">
+                Web Mill
+            </div>
+            <a href="/index.php" class="nested" id="navbar-home-link">
+                home
+            </a>
+            <a href="/admin.php" class="nested" id="navbar-admin-link">
+                admin
+            </a>
+        </div>
+        <h1 class="nested" id="main-text">
+            Welcome to your website!
+        </h1>
+        <div class="nested" id="footer">
+            <div class="nested" id="copyright">
+                <div class="nested" id="copyright-text">
+                    Copyright&nbsp;©&nbsp;
+                </div>
+                <div class="nested" id="copyright-year">
+                    2019
+                </div>
+                <div class="nested" id="copyright-name">
+                    &nbsp;Your Name Here
+                </div>
+            </div>
+        </div>
     </body>
 </html>
