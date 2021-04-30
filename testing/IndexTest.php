a6e6272a97f86c99af56b4363bd1073498f73c4d
Austin Jacob <ajacob779@gmail.com>
2021-04-30 13:11:31 -0600

    Update IndexTest.php

--- testing/IndexTest.php
+++ testing/IndexTest.php
@@ -9,6 +9,7 @@ class IndexTest extends TestCase
     //Temporary test case, just to know that PHPUnit was set up properly:
     public function testFaviconExists()
     {
-        $this->assert(file_exists($_SERVER['DOCUMENT_ROOT'] . '/favicon.ico'));
+        $favicon = file_exists($_SERVER['DOCUMENT_ROOT'] . '/favicon.ico');
+        $this->assertEquals($favicon, true);
     }
 }
